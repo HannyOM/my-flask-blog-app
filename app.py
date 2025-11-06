6aa71f43852d02ac1e78bf0514877a815c1ccb6d
HannyOM <hanny0568@gmail.com>
2025-11-06 13:18:00 +0100

    added add route and updated index route and post class

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bloggr.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    post = db.Column(db.String, nullable=False)
-    author = db.Column(db.String)
+    title = db.Column(db.String)
+    post = db.Column(db.String)
+    # author = db.Column(db.String)
     date = db.Column(db.Date)
 
 with app.app_context():
@@ -18,7 +20,8 @@ with app.app_context():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_posts = Post.query.all()
+    return render_template("index.html", all_posts=all_posts)
 
 
 @app.route("/new")
@@ -26,13 +29,14 @@ def new():
     return render_template("new.html")
 
 
-@app.route("/save", methods=["GET", "POST"])
-def save():
-    # user clicks add post button
-    # if request method is post, 
-    # get the value of title
-    # get the value of post
-    # 
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    title = request.form.get("post_title")
+    post = request.form.get("post_content")
+    if title and post:
+        new_post = Post(title=title, post=post, date=datetime.utcnow())
+        db.session.add(new_post)
+        db.session.commit()
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
