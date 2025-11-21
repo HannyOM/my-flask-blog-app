80b9c1a6605e2dd63f96861cc451ed1cdd75d1c5
HannyOM <hanny0568@gmail.com>
2025-11-21 16:10:09 +0100

    Initialized User database model

--- bloggr/models.py
+++ bloggr/models.py
@@ -7,3 +7,7 @@ class Post(db.Model):
     author = db.Column(db.String)
     date = db.Column(db.Date)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
