f2115d810beb2470b93c53d07248192f64781d03
HannyOM <hanny0568@gmail.com>
2025-11-21 16:09:10 +0100

    Added auth.register view & Updated auth_bp

--- bloggr/auth.py
+++ bloggr/auth.py
@@ -1,9 +1,35 @@
-from flask import Blueprint 
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from . import db, bcrypt
 
 auth_bp = Blueprint("auth",         # Name of the blueprint.
                     __name__,           # Lets the blueprint know where it is defined.
-                    url_prefix="/auth")         # Prepends to all URLs associated to the blueprint.
+                    url_prefix="/auth",
+                    template_folder="templates")         # Prepends to all URLs associated to the blueprint.
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods=["GET", "POST"])
 def register():
-    return "Registration Page"
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        
+        if error is None:
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                error = "User already exists."
+            else:
+                hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+                new_user = User(username=username, password=hashed_password) # type: ignore
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for("hello"))           # TO BE CHANGED
+        
+        flash(error)
+
+    return render_template("auth/register.html")
