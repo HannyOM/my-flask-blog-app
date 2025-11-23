10ddc2c1b6d891c4cbe8a6b15d72c37fc1c306d8
HannyOM <hanny0568@gmail.com>
2025-11-23 09:23:10 +0100

    Added UserMixin to User class

--- bloggr/models.py
+++ bloggr/models.py
@@ -1,4 +1,5 @@
 from . import db
+from flask_login import UserMixin
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,7 +8,7 @@ class Post(db.Model):
     author = db.Column(db.String)
     date = db.Column(db.Date)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(100), nullable=False)
