eb816df7622e7234efab165153f58373eded3d24
HannyOM <hanny0568@gmail.com>
2025-11-27 21:14:30 +0100

    Updated Post table author_id column and User-post relationship.

--- bloggr/models.py
+++ bloggr/models.py
@@ -1,14 +1,18 @@
 from . import db
 from flask_login import UserMixin
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String, nullable=False)
-    author = db.Column(db.String)
-    date = db.Column(db.Date)
-
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin):            # Initializes a "User" table with three columns.
     id = db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)            # Creates a relationship to the Post model so a user can have many posts.
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)            # Initializes a "Post" table with five columns.
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False) # "ForeignKey" points to the "id" column in the "User" table.
+    date = db.Column(db.Date)
+
+
+
