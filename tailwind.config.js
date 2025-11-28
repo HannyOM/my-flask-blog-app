f76a4967e3311aaf753d07e11dfb10eb9b70294c
HannyOM <hanny0568@gmail.com>
2025-11-28 20:15:34 +0100

    Added Tailwindcss and its configurations.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./bloggr/templates/**/*.html",
+    "./bloggr/static/**/*.js",
+    "./bloggr/src/**/*.css", 
+    "./src/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
