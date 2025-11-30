80874a993c187729fb7cdbc5f1ee9e75213976af
HannyOM <hanny0568@gmail.com>
2025-11-30 20:51:16 +0100

    Converted config file to ESM and imported DaisyUI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 import daisyui from 'daisyui';
 
 export const content = [
@@ -11,14 +10,3 @@ export const theme = {
 export const plugins = [
   daisyui
 ];
-=======
-module.exports = {
-  content: [
-    "./bloggr/templates/**/*.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> e39d8df9c7e88f96c23908c7f71b64da1aaf65b5
