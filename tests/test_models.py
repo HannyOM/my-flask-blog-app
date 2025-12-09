e7ff0355b30a6eb3dce01a5ad02a462c75094337
HannyOM <hanny0568@gmail.com>
2025-12-09 01:52:31 +0100

    Added 'test_create_user', 'test_create_post', 'test_post_author_relationship', 'test_user_posts_relationship'

---
+++ tests/test_models.py
@@ -0,0 +1,111 @@
+from bloggr.models import User, Post
+from datetime import date
+
+
+def test_create_user(app, db, bcrypt):          # Ensures a user can be created and added to the database.
+    with app.app_context():         
+        # Create a user and add to the database.
+        username = "Hanny_username"
+        hashed_password = bcrypt.generate_password_hash("Hanny_password").decode("utf-8")
+        user = User(username=username, password=hashed_password) # type: ignore
+        db.session.add(user)
+        db.session.commit()
+
+        # Retreive the user and create assertions on the details.
+        existing_user = User.query.filter_by(username=username).first()
+        assert existing_user is not None
+        assert existing_user.id is not None
+        assert bcrypt.check_password_hash(existing_user.password, "Hanny_password") == True 
+
+
+def test_create_post(app, db, bcrypt):         # Ensures a post can be created, added to a database and linked to a user.
+    with app.app_context():
+        # Create a user and add to the database.
+        username = "Abubakar_username"
+        hashed_password = bcrypt.generate_password_hash("Abubakar_password").decode("utf-8")
+        user = User(username=username, password=hashed_password) # type:ignore
+        db.session.add(user)
+        db.session.commit()
+
+        # Create a post and add to the database.
+        post = Post(
+            title = "My First Blog Post",           # type:ignore
+            content = "Jokes aside, this is actually my first blog post.",          # type:ignore
+            author_id = user.id,            # type:ignore
+            date = date.today()         # type:ignore
+        )
+        db.session.add(post)
+        db.session.commit()
+        
+        # Retrieve the post and make assertions on the post details.
+        existing_post = Post.query.filter_by(title="My First Blog Post").first()
+        assert existing_post is not None
+        assert existing_post.id is not None
+        assert existing_post.content == "Jokes aside, this is actually my first blog post."
+        assert existing_post.author_id == user.id
+        assert existing_post.date == date.today()
+
+
+def test_post_author_relationship(app, db, bcrypt):         # Ensures existing_post.author returns the User instance (backref works).
+    with app.app_context():
+        # Create a user and add to the database.
+        username = "Oshiozokhai_username"
+        hashed_password = bcrypt.generate_password_hash("Oshiozokhai_password").decode("utf-8")
+        user = User(username=username, password=hashed_password) # type:ignore
+        db.session.add(user)
+        db.session.commit()
+
+        # Create a post and add to the database.
+        post = Post(
+            title = "My Second Blog Post",           # type:ignore
+            content = "Really, this is actually my second blog post.",          # type:ignore
+            author_id = user.id,            # type:ignore
+            date = date.today()         # type:ignore
+        )
+        db.session.add(post)
+        db.session.commit()
+        
+        # Retrieve the post and make assertions on the post_author relationship.
+        existing_post = Post.query.filter_by(title="My Second Blog Post").first()
+        assert existing_post.author == user         # type: ignore
+        assert existing_post.author.username == username            # type: ignore
+
+
+def test_user_posts_relationship(app, db, bcrypt):         # Ensures existing_user.posts returns all posts authored by the user.
+    with app.app_context():
+        # Create a user and add to the database.
+        username = "Mustapha_username"
+        hashed_password = bcrypt.generate_password_hash("Mustapha_password").decode("utf-8")
+        user = User(username=username, password=hashed_password) # type:ignore
+        db.session.add(user)
+        db.session.commit()
+
+        # Create multiple posts and add to the database.
+        post_one = Post(
+            title = "My First Blog Post",           # type:ignore
+            content = "Really, this is actually my first blog post.",          # type:ignore
+            author_id = user.id,            # type:ignore
+            date = date.today()         # type:ignore
+        )
+        post_two = Post(
+            title = "My Second Blog Post",           # type:ignore
+            content = "Really, this is actually my second blog post.",          # type:ignore
+            author_id = user.id,            # type:ignore
+            date = date.today()         # type:ignore
+        )
+        post_three = Post(
+            title = "My Third Blog Post",           # type:ignore
+            content = "Really, this is actually my third blog post.",          # type:ignore
+            author_id = user.id,            # type:ignore
+            date = date.today()         # type:ignore
+        )
+        db.session.add_all([post_one, post_two, post_three])
+        db.session.commit()
+
+        # Retrieve the user details and make assertions on the user_posts relationship.
+        existing_user = User.query.filter_by(username=username).first()
+        assert len(existing_user.posts) == 3            # type:ignore
+        assert [each_post.title for each_post in existing_user.posts] == ["My First Blog Post",         # type:ignore
+                                                                 "My Second Blog Post",
+                                                                 "My Third Blog Post"]
+        # assert that the titles of all the user's posts are what we defined. 
