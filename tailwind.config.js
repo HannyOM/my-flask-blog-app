88fbbbb8d19a41b87a0b54caf408cef9db7af61a
HannyOM <hanny0568@gmail.com>
2025-12-17 23:43:48 +0100

    Updated tailwindcss and daisyui configurations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
-/** @type {import('tailwindcss').Config} */         // Tells the editor that this file follows the Tailwind CSS config type. Enables autocomplete, type checking, and intellisense for Tailwind settings.   
-import daisyui from 'daisyui';          // Tells Tailwind which files to scan for class names.
+/** @type {import('tailwindcss').Config} */     // Tells the editor that this file follows the Tailwind CSS config type. Enables autocomplete, type checking, and intellisense for Tailwind settings.
+import daisyui from "daisyui"
 
-export const content = [          // Defines your Tailwind design system (colors, spacing, fonts, breakpoints).
-  "./**/*.html",
-];
-export const theme = {          // Lets you add new values instead of overriding existing ones.
-  extend: {},         // Lets you register Tailwind plugins.
-};
-export const plugins = [          // Lets you register Tailwind plugins.
-  daisyui
-];
-
-
-
-
-
-
+export default {      
+  content: [      // Tells Tailwind which files to scan for class names.
+    "./**/*.html",
+  ],
+  theme: {      // Defines your Tailwind design system (colors, spacing, fonts, breakpoints).
+    extend: {},     // Lets you add new values instead of overriding existing ones.
+  },
+  plugins: [      // Lets you register Tailwind plugins.
+    daisyui,
+  ],
+}
