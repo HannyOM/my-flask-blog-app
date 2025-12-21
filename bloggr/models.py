561e5c1c27489970b88cbee458127664db43ca52
HannyOM <hanny0568@gmail.com>
2025-12-21 02:01:28 +0100

    Added PostgreSQL database table names.

--- bloggr/models.py
+++ bloggr/models.py
@@ -2,12 +2,14 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):            # Initializes a "User" table with three columns.
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     posts = db.relationship("Post", backref="author", lazy=True)            # Creates a relationship to the Post model so a user can have many posts.
 
 class Post(db.Model):
+    __tablename__ = "post"
     id = db.Column(db.Integer, primary_key=True)            # Initializes a "Post" table with five columns.
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String, nullable=False)
