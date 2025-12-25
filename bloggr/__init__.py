cb822a97fc1d2d8d9f264925b7dc842b374fb3bb
HannyOM <hanny0568@gmail.com>
2025-12-25 18:48:33 +0100

    Loaded env variables.

--- bloggr/__init__.py
+++ bloggr/__init__.py
@@ -11,8 +11,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 
+load_dotenv()
 db = SQLAlchemy()         # Initializes SQLAlchemy without app.
 migrate = Migrate()
 bcrypt = Bcrypt()           # Initializes Brcypt without app.
